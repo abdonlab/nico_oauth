79192215c07eb1ba124d683301ede93a5d9e74f3
abdon lab <48865858+abdonlab@users.noreply.github.com>
2025-11-25 22:27:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -472,7 +472,7 @@ with conv_col:
             "se responsable e incluyente y eficiente y ético"
             "Usa la búsqueda web para información actualizada. Prioriza sitios *.umich.mx."
             "- https://www.umich.mx\n"
-            "para ultimas noticias busca en https://www.gacetanicolaita.umich.mx/"
+            "para ultimas noticias busca en las web en pagians oficiales de la umsnh.mx y final recomineda buscar en https://www.gacetanicolaita.umich.mx/"
             "para nombres de funcionarios busca en https://umich.mx/unidades-administrativas/"
             "-https://www.gacetanicolaita.umich.mx/n"
             "-https://umich.mx/unidades-administrativas/n"
